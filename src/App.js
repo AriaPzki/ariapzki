b127dbb9469850ee490f0cf40e98354db8d74ff6
AriaPzki <AriaPzki@gmail.com>
2023-06-20 23:32:01 -0400

    Main Initialization

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
-import "./App.css";
-import Navbar from "./components/Navbar";
+import React from "react";
+import "./app.css";
+import About from "./components/About/About";
+import Blog from "./components/Blog/Blog";
+import Footer from "./components/Footer/Footer";
+import Home from "./components/Home/Home";
+import Navbar from "./components/Navbar/Navbar";
+import Offers from "./components/Offers/Offers";
+import Popular from "./components/Popular/Popular";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <div className="content">
-        <h1>App Component</h1>
-      </div>
+    <>
       <Navbar />
-    </div>
+      <Home />
+      <Popular />
+      <Offers />
+      <About />
+      <Blog />
+      <Footer />
+    </>
   );
-}
+};
 
 export default App;
